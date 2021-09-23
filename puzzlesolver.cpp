1b1688a117cde3aa0ad6928c3df12e3583c0c126
evasolp <evap19@ru.is>
2021-09-23 11:40:39 +0000

    Blee

--- puzzlesolver.cpp
+++ puzzlesolver.cpp
@@ -14,6 +14,53 @@
 
 using namespace std;
 
+int open_socket(){
+    int udp_sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+        if(udp_sock < 0) // -1 if error
+        {
+            perror("Failed to open socket");
+            return(-1);
+        }
+    return udp_sock;
+}
+
+string send_recv(const char* IP, int port, char* buffer, struct sockaddr_in destaddr){
+    char message_buffer[1400];
+    memset(&message_buffer, 0, sizeof(message_buffer));
+    string return_messages;
+    int udp_sock = open_socket();
+    if(udp_sock > 0){
+        fd_set masterfds;
+        FD_SET(udp_sock, &masterfds);
+        struct timeval timeout;             // Timeout for recvfrom()
+        timeout.tv_sec = 0;
+        timeout.tv_usec = 20000;            // Set timeout to 0.2 seconds
+        
+
+        destaddr.sin_family = AF_INET;
+        inet_aton(IP, &destaddr.sin_addr);
+        destaddr.sin_port = htons(port);
+
+        if (sendto(udp_sock, buffer, strlen(buffer), 0, (const struct sockaddr *)&destaddr, sizeof(destaddr)) < 0){
+            perror("Failed to send");
+        }
+        else {
+            int t = select(udp_sock + 1, &masterfds, NULL, NULL, &timeout);
+            if (t > 0){ // if t is 0, timeout accured
+                int destaddr_size = sizeof(destaddr);
+                if(recvfrom(udp_sock, message_buffer, sizeof(message_buffer), 0, (sockaddr *)&destaddr, (socklen_t *)&destaddr_size) < 0){
+                    perror("Failed to recieve");
+                }
+                else { // if ok, print buffer messages
+                    return_messages = message_buffer;
+                    return return_messages;
+                }
+            }
+        }
+    }
+    return return_messages;
+}
 
 
 
@@ -23,42 +70,45 @@ int main(int argc, char *argv[]){
     char buffer[1400];  // Buffer for information to send
     int length;
     struct sockaddr_in destaddr;
+    string messages;
+    const char *IP;
+
 
     set<int> open_ports;
 
-    strcpy(buffer, "Hi Port!"); // Message set to buffer
+    strcpy(buffer, "$group_37$"); // Message set to buffer
     length = strlen(buffer) + 1; // lenght of buffer
 
     if (argc == 2){
-        const char *IP = argv[1];
+        IP = argv[1];
         //call scanner to get the open ports that are not hidden
         Scanner port_scanner = Scanner(IP, 4000, 4100);
         int udp_sock = port_scanner.open_socket();
         destaddr.sin_family = AF_INET;
         inet_aton(IP, &destaddr.sin_addr);
         int destaddr_size = sizeof(destaddr);
+        
 
         open_ports = port_scanner.the_scanner();
-        for (int i : open_ports){
-            cout << "open ports: " << i << endl;
-        }
 
     }
     else if (argc == 6){
         //got the ports in as arguments
         // send the oracle a comma-seperated list of the hidden ports, and it will show us the way 4042
         // Send a message containing $group_#$ where # is our group number 4096
-        const char *IP = argv[1];
+        IP = argv[1];
         for (int i = 2; i < 6; i++){
-            open_ports.insert(argv[i]);
+            open_ports.insert(atoi(argv[i]));
         }
-        for (auto i : open_ports){
-            cout << "open ports: " << i << endl;
-        }
-        
-
 
 
     }
+    for (int port : open_ports){
+            messages = send_recv(IP, port, buffer, destaddr);
+            while (messages == ""){
+                messages = send_recv(IP, port, buffer, destaddr);
+            }
+            cout << port << " : " << messages << endl;
+        }
     return 0;
 }
